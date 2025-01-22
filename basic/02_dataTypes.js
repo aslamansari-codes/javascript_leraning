6535caca8e0b33f5464cc5cf51a02e13ddf2239e
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-01-22 19:41:57 +0000

    datatypes notes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,51 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // We are using Nodejs, Not Browser so this is not possible here
+
+//console.log(5+3);  console.log("My GGV"); //Not good manner in the programing beacuse lack of code readability
+
+/*
+console.log(5+3);
+                        // These are good practice in coding
+console.log("Rakesh");
+*/
+
+
+/*
+let name = "Aslam Ansari"
+let age = 22
+let isLoggedIn = true
+*/
+
+
+
+// above declaration variables assign many diffrent types data type like number, boolean etc.
+
+/* Data Types in js :-
+
+number => 2 to power 53 range
+bigint => mostly used in Trading, Stock Market, facebook etc.
+string => ""
+boolean => true/false
+null => it is a standalone value & representation of empty value like let roomTempreture = null;
+undefined => it means not anything assign variable. like let num1;
+symbol => used uniqueness
+
+object =>
+*/
+ let newAge = 20
+  console.log(typeof newAge); //number
+  let value = null
+  console.log(typeof value); // but is gives the object data type because language error
+
+  let city;
+  console.log(typeof city); // undefined
+  
+  
+  
+
+
+
+
+
+
--- basic/operation.js
+++
@@ -1,9 +0,0 @@
-let value = 3
-let negValue = -value
-console.log(negValue)
-// increment(++)
-let x = 4
-let y = x++;
-console.log(x);
-console.log(y);
-
