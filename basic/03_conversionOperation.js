9616080df56f83ef8746e56cb0f8cb211627dbcf
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-01-24 18:45:38 +0000

    conversion of data type

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,47 @@
+
+/*let score = "33aby"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);*/
+
+// ******************************----------------------------****************
+// Converte following literals in form of Number Data Type then result is :-
+// "33" => 33
+// "33ab" => NaN (Not a Number)
+// true => 1; false => 0;
+// "Aslam" => NaN
+
+//--------------------------------------------------------
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanLoggedIn);
+
+// 1 => true 0  => false
+
+// "" => flase  "Aslam" => true
+
+let intNumber = 75
+
+//console.log(intNumber); // 75 in form for Number
+//console.log(typeof intNumber);  // Number Type
+
+
+let stringNumber = String(intNumber)
+//console.log(stringNumber);  // 75 but it is String
+
+//console.log(typeof stringNumber);  // String type
+
+
+
+
+
+
+
+
+
