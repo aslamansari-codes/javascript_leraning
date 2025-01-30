de2bf7580c01b3692eff7fda6a71be537d1dc56a
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-01-30 18:22:25 +0000

    datatype_summary

---
+++ basic/datatype-summry.js
@@ -0,0 +1,37 @@
+// Primitive Data type
+/* 7 Types :- => String known as call by value
+// String 
+  Number:-  let score = 10, const scoreValue = 100.3;
+  Boolean :- let isLoggedIn = false;
+  Null :-  let outsideTemp = null;    // return type of null is Object
+  Undefined :- let userEmail;
+  Symbol :- let id = Symbol('123') //  Return type of id is Symbol
+            let anotherId = Symbol('123')
+            console.log(id===anotherId);  // false, because symbol provide indiviual vairiables
+  BigInt :- let myNumber = 3525599896566n
+
+  */
+
+  // Reference Type ( Non-Primitive type) :-
+ /*
+   Array :- const myHero = ["Balveer","Saktiman","Nagraj"];  //  Return type of myHero is Object
+
+
+   Object :- const myObj = {                                              
+        name : "Aslam",                 //  Return type of myObj is Object
+        age : 21,
+   }
+   Function  :-  const myFunction = function(){   
+
+    console.log("Hello nerds");             // Return type of myFunction is Object Function
+   }
+ */
+
+
+
+
+
+   
+
+
+
