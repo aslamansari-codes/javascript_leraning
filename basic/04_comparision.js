7951e354e6687fc0da693b252f0ff664160cacee
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-01-30 17:42:37 +0000

    new changes

--- basic/04_comparision.js
+++ basic/04_comparision.js
@@ -11,9 +11,23 @@
 // console.log("02">1);  // true
 // console.log("2"<1);  // true
 
-console.log(null>0);  //false
+/*console.log(null>0);  //false
 console.log(null==0);  // false
 console.log(null>=0);  // true beacuse here  type conversion
+// */
+
+/*console.log(undefined==0);
+console.log(undefined>0);         // avoid this type of conversion
+console.log(undefined<0);*/
+
+console.log("2"===2);   // false becuse === equal to check strictly type
+
+
+
+
+
+
+
 
 
 
