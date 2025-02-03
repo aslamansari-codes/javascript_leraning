29d6ec09146c931064ef47b66034427700c98bfc
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-02-03 21:14:13 +0000

    new add string demo

---
+++ basic/06_strings.js
@@ -0,0 +1,34 @@
+// const name = "Aslam"
+// let repoCount = 50
+
+//  console.log(name+repoCount+ " Values"); it is not recommandable
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // using backtick string entupulation
+
+const gameName = new String('Aslam Ansari')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('A'));
+
+// let newString = gameName.substring(0,5)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-12,5)
+// console.log(anotherString);
+
+
+
+
+
+
+
+
+
+
+
+
+ 
