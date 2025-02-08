57891ffe9d22858717aaa75684d83b9bc7ca7584
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-02-08 08:50:00 +0000

    math and num disscuss

--- basic/07_numsandmath.js
+++ basic/07_numsandmath.js
@@ -16,15 +16,73 @@ console.log(balance.toString().length);  // 4
 
 */
 
-const otherNumber = 23.8956
+/*
 
-console.log(otherNumber.toPrecision(2));  //  24
-
-console.log(otherNumber.toPrecision(3));    // 23.9
+ const otherNumber = 23.8956
+ console.log(otherNumber.toPrecision(2));    24
+ console.log(otherNumber.toPrecision(3));     23.9
  
-console.log(otherNumber.toPrecision(4));     //23.90
+ console.log(otherNumber.toPrecision(4));     23.90
+ console.log(otherNumber.toPrecision(1));     2e+1
 
-console.log(otherNumber.toPrecision(1));     // 2e+1
+ */
+
+
+/*
+ const hundreds = 1000000
+console.log(hundreds.toLocaleString());  //  1,000,000 this is US standard value
+console.log(hundreds.toLocaleString('en-IN'));   // 10,00,000 this is Indian standard value
+
+*/
+
+// ++++++++++++++++++++++ Maths +++++++++++++++
+
+// console.log(Math);  //  Object [Math] {}
+// console.log(Math.abs(-4));   // 4
+// console.log(Math.abs(4));    // 4
+// console.log(Math.round(5.8));   //6
+// console.log(Math.round(5.4));   // 5
+// console.log(Math.ceil(4.2));  //5
+// console.log(Math.ceil(7.6));   // 8
+// console.log(Math.floor(9.1));   //9
+// console.log(Math.floor(9.9));   //9
+// console.log(Math.sqrt(36));   //6
+// console.log(Math.pow(5,2));   // 25
+// console.log(Math.min(4,10,5,-6,85));  //-6
+// console.log(Math.max(8,4,5,15,65,25,8,-9,-100));  //65
+
+console.log(Math.random());  // return the random values betwwen 0 and 1
+console.log((Math.random()*10)+1);  // return greater than 1 values
+console.log(Math.floor((Math.random()*10)+1));  // return only one digit value
+
+// need customize range value
+
+const min = 10
+const max = 20
+// formula for range value
+console.log(Math.floor(Math.random()*(max-min+1))+min); 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
