5db3ac288ce202270cd49d3565f3a79b76da5e8d
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-02-09 15:55:38 +0000

    objects part2 introduction

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,88 @@
+// const tindUser = new Object()
+
+ const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+// const regularUser = {
+//     email: "aslamdevmth@chatgpt.com",
+//     fullName: {
+//         userFullName: {
+
+//             firstName: "Aslam",
+//             lastName: "Ansari"
+
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullName.userFullName);  // { firstName: 'Aslam', lastName: 'Ansari' }
+
+// combine or merge object
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+
+// console.log(obj3);  // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+// const obj4 = Object.assign({}, obj1, obj2)
+
+// console.log(obj4);  // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+// const obj5 = {...obj1,...obj2}
+// console.log(obj5);  // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+const user =  [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "b@gmail.com"
+    },
+    {
+        id: 3,
+        email: "c@gmail.com"
+    },
+    {
+        id: 4,
+        email: "d@gmail.com"
+    }
+    
+    
+]
+
+// console.log(user[1].email);  //  b@gmail.com
+// console.log();
+
+console.log(tinderUser);  // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+console.log(Object.keys(tinderUser));  // [ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.values(tinderUser));  // [ '123abc', 'Sammy', false ]
+
+console.log(Object.entries(tinderUser));  // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true
+console.log(tinderUser.hasOwnProperty('isLogged'));   //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
