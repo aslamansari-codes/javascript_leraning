6f111b5efaa371f4502912150bf6c8b3c790f215
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-02 17:22:36 +0000

    basics of for loops and break continue

--- 05_iterations/01_loops.js
+++ 05_iterations/01_loops.js
@@ -10,16 +10,47 @@
 //     console.log(element);
 // }
 
-for (let i = 1; i <=10; i++) {
-    console.log(`Outer loop ${i}`);
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop ${i}`);
     
-   for (let j = 1; j <=10; j++) {
-    // console.log(`Inner loop ${j} and inner loop ${i}`);
-    console.log(`${i} * ${j} = ${i*j}`);
+//    for (let j = 1; j <=10; j++) {
+//     // console.log(`Inner loop ${j} and inner loop ${i}`);
+//     console.log(`${i} * ${j} = ${i*j}`);
+    
+//    }
+    
+// }
+
+// const myArray = ["flash", "Batman","Superman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+//  break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index==5) {
+
+//         console.log(`Detected 5`);
+//         break
+        
+//     }
+//     console.log(`Values of index is ${index}`);
     
     
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index==5) {
+
+        console.log(`Detected 5`);
+        continue
+        
+    }
+    console.log(`Values of index is ${index}`);
     
-   }
     
 }
-
