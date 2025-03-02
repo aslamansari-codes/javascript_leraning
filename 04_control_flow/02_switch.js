1098f101ad75f489f58a968f7c31d05715187eaf
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-02 08:30:49 +0000

    fundamental of switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,26 @@
+// Switch Statement //
+
+const month = "april"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Invalid month");
+        
+        break;
+}
