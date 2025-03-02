a88b04e5c939181903c11324b7e96ebf4f426018
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-02 17:42:55 +0000

    while & do-while loops

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,30 @@
+// ++++++++++ While Loop ++++++++++++
+
+// let index = 0
+
+// while (index<20) {
+//     console.log(`Value of index is ${index}`);
+//     index = index+2
+// }
+
+// const myArray = ["flash", "Batman","Superman"]
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+
+//     console.log(`Value is ${myArray[arr]}`);
+
+//     arr = arr+1
+    
+// }
+
+
+// ++++++++++++ do-while loop ++++++++++++
+
+let score = 1
+
+do {
+   console.log(`Score is ${score}`);
+   score = score+1  
+} while (score<=10);
