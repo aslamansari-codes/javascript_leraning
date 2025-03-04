31b30ef573b67b9dfcf2006945a5370b20267191
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-04 17:45:22 +0000

    fundmentals of forof loop

--- 05_iterations/03_loops.js
+++ 05_iterations/03_loops.js
@@ -13,6 +13,66 @@ for (const greet of greetings) {
     if (greet ==" ") {
         continue
     }
-    console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
     
+}
+
+
+//  ++++++++++ Map  ++++++++++
+
+const map = new Map()
+
+map.set("IN", "India")
+map.set("USA", "United State of America")
+map.set("FR", "France")
+map.set("IN", "India")
+
+// console.log(map);
+
+// ++++++++ output  ++++++++++
+/*
+Map(3) {
+  'IN' => 'India',
+  'USA' => 'United State of America',
+  'FR' => 'France'
+}
+*/
+
+for (const key of map) {
+    // console.log(key);
+    
+}
+
+// +++++++ output  ++++++++++++
+
+/*
+
+[ 'IN', 'India' ]
+[ 'USA', 'United State of America' ]
+[ 'FR', 'France' ]
+
+*/
+
+for (const [key, value] of map) {
+    // console.log(key, ":-", value);
+    
+}
+
+ // ++++  output +++++
+
+ /*
+
+IN :- India
+USA :- United State of America
+FR :- France
+
+
+ */
+
+const myObject = {
+    game1: "Spiderman",
+    game2: "Flash"
+}
+for (const [key, value] of myObject) {
+    console.log(key, ":-", value);    // myObject is not iterable
 }
