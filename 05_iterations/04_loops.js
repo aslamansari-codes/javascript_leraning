6fc26cb63db5e5d1e0ea9e81f3dacfc807b00a1b
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-06 11:12:39 +0000

    basics of forin loop

--- 05_iterations/04_loops.js
+++ 05_iterations/04_loops.js
@@ -4,3 +4,69 @@ const myObject = {
     rb: "ruby",
     swift: "swift of apple"
 }
+
+for (const key in myObject) {
+    
+    //console.log(key);
+     
+    }
+
+// +++++++ output +++++++++
+/*
+js
+cpp
+rb
+swift
+*/
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+      
+    }
+
+// ++++++++  output  ++++++++++
+
+/*
+js shortcut is for Javascript
+cpp shortcut is for C++
+rb shortcut is for ruby
+swift shortcut is for swift of apple
+*/
+
+const programming = ["js","py","rb","java","cpp","c"]
+
+for (const key in programming) {
+   //console.log(key);
+   
+}
+
+// ++++++++++  output +++++++++ 
+
+/*
+0
+1
+2
+3
+4
+5
+*/
+
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
+
+// +++++++ output +++++++++
+
+/*
+js
+py
+rb
+java
+cpp
+c
+*/
+
+
+
+
