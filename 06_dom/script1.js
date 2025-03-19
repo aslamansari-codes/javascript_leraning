67dd63f3d9b00260c435be4dfbd657e35b77b9fd
Aslam776688 <137589830+Aslam776688@users.noreply.github.com>
2025-03-19 06:07:32 +0000

    dom basics

---
+++ 06_dom/script1.js
@@ -0,0 +1,16 @@
+// document.getElementById('title').innerHTML = '<h1>Desi Coder</h1>' // Desi Coder
+
+// const body = document.getElementById('bd');
+
+// body.style.color = "yellow";
+// body.style.backgroundColor = 'blue';
+//  const title = document.getElementById('title')
+//   title.setAttribute('class', 'newtest')
+//   title.style.backgroundColor = 'green'
+//   title.style.color = 'white'
+//   title.textContent
+
+/*
+
+
+*/
