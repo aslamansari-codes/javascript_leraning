d4c86f7afa10128f25e2c0b297eb362508e7c52b
Aslam Ansari <137589830+aslamansari-codes@users.noreply.github.com>
2025-04-15 12:35:53 +0000

    about object literals

--- 10_OOPS/01_oop.js
+++ 10_OOPS/01_oop.js
@@ -1,5 +1,5 @@
 const user = {
-    username: "hitesh",
+    username: "aslam",
     loginCount: 8,
     signedIn: true,
 
@@ -31,7 +31,7 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 
-const userOne = new User("hitesh", 12, true)
-const userTwo = new User("ChaiAurCode", 11, false)
+const userOne = new User("aslam", 12, true)
+const userTwo = new User("selfCode", 11, false)
 console.log(userOne.constructor);
 //console.log(userTwo);
